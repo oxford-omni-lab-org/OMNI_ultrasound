9d893103b8d9de62eb0e5e6bfc569bc7cb53b427
lindehesse <lindehesse@gmail.com>
2023-12-15 11:29:43 +0000

    added additional tests for the subcortical segmentation part

--- Tests/test_subcortical_segmentation.py
+++ Tests/test_subcortical_segmentation.py
@@ -2,10 +2,13 @@ import torch
 from fetalbrain.structural_segmentation.subcortical_segm import (
     load_segmentation_model,
     segment_subcortical,
+    segment_scan_subc,
+    keep_largest_compoment,
+    compute_volume_segm,
 )
 from fetalbrain.structural_segmentation.segmentation_model import UNet
 from fetalbrain.utils import read_image
-from fetalbrain.alignment.align import load_alignment_model, align_to_atlas, prepare_scan
+from fetalbrain.alignment.align import load_alignment_model, align_to_atlas, prepare_scan, align_scan
 from path_literals import TEST_IMAGE_PATH, TEST_SEGM_PATH
 import numpy as np
 
@@ -14,16 +17,6 @@ def test_load_segmentation_model() -> None:
     model = load_segmentation_model()
     assert isinstance(model, torch.nn.DataParallel)
     assert isinstance(model.module, UNet)
-
-
-def test_prepare_scan_segm() -> None:
-    example_scan, _ = read_image(TEST_IMAGE_PATH)
-    torch_scan = prepare_scan(example_scan)
-    assert torch_scan.shape == (1, 1, 160, 160, 160)
-
-    # make sure it is scaled between 0 and 255
-    assert torch.max(torch_scan) > 1
-    assert torch.min(torch_scan) >= 0
 
 
 def test_segment_subcortical() -> None:
@@ -51,3 +44,52 @@ def test_segment_subcortical() -> None:
 
     assert ref_segm.shape == multi_class_np.shape
     assert np.allclose(ref_segm, multi_class_np, atol=1e-4)
+
+
+def test_segment_scan_subc() -> None:
+    example_scan, _ = read_image(TEST_IMAGE_PATH)
+    aligned_scan, _ = align_scan(example_scan)
+
+    segm, keys = segment_scan_subc(aligned_scan, connected_component=False)
+    assert segm.shape == (160, 160, 160)
+
+    ref_segmpath = TEST_SEGM_PATH / "ref_segmap.nii.gz"
+    ref_segm, _ = read_image(ref_segmpath)
+
+    assert ref_segm.shape == segm.shape
+    assert np.allclose(ref_segm, segm, atol=1e-4)
+
+
+def test_keep_largest_component() -> None:
+    # get a segmentation
+    ref_segmpath = TEST_SEGM_PATH / "ref_segmap.nii.gz"
+    ref_segm, _ = read_image(ref_segmpath)
+
+    # check that it doesn't do anything for segm without unconnected
+    assert np.all(ref_segm == keep_largest_compoment(ref_segm))
+
+    # add a unconnected component to the label map
+    segm_cc = ref_segm.copy()
+    segm_cc[0:2, :2, :2] = 1
+    assert not np.allclose(segm_cc, ref_segm, atol=1e-4)
+
+    # prcoess this to remove the component
+    segm_cc_rem = keep_largest_compoment(segm_cc)
+
+    # check that the unconnected component was removed
+    assert np.all(segm_cc_rem == ref_segm)
+
+
+def test_compute_volume_segm() -> None:
+    ref_segmpath = TEST_SEGM_PATH / "ref_segmap.nii.gz"
+    ref_segm, spacing = read_image(ref_segmpath)
+
+    # compute volume of each class
+    key_maps = {"ChP": 1, "LPVH": 2, "CSPV": 3, "CB": 4}
+    volume_dict = compute_volume_segm(ref_segm, key_maps=key_maps, spacing=spacing)
+
+    assert volume_dict.keys() == key_maps.keys()
+    assert np.isclose(volume_dict["ChP"], 0.692, atol=1e-3)
+    assert np.isclose(volume_dict["LPVH"], 0.179, atol=1e-3)
+    assert np.isclose(volume_dict["CSPV"], 0.142, atol=1e-3)
+    assert np.isclose(volume_dict["CB"], 1.696, atol=1e-3)
--- src/fetalbrain/model_paths.py
+++ src/fetalbrain/model_paths.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-MODEL_WEIGHTS_FOLDER = Path("src/model_weights")
+MODEL_WEIGHTS_FOLDER = Path(__file__).parent.parent / Path("model_weights")
 BAN_MODEL_PATH = MODEL_WEIGHTS_FOLDER / "alignment" / "fBAN_modelweights.pt"
 SEGM_MODEL_PATH = MODEL_WEIGHTS_FOLDER / "subc_segmentation" / "subc_segm.tar"
 TEDS_MULTI_MODEL_PATH = MODEL_WEIGHTS_FOLDER / 'teds_segmentation' / 'finalmodel_multistructure.pt'
 SIDE_DETECTOR_MODEL_PATH = MODEL_WEIGHTS_FOLDER / 'teds_segmentation' / 'FinalModel_sidedetection.pt'
 PRIOR_SHAPE_PATH = MODEL_WEIGHTS_FOLDER / 'teds_segmentation' / '26wks_AllLabels.mha'
-
